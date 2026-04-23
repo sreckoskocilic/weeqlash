a22c5786eb94c408d2dac3d7089c9179f6220ae0
Srecko Skocilic <srecko.skocilic@gmail.com>
2026-04-23 10:10:05 +0200

    capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'weeqlash.app',
+  appName: 'Weeqlash',
+  webDir: 'client'
+};
+
+export default config;
